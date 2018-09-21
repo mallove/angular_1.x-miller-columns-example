088d720b669d6bcb55729198f07e421ecbf1c21a
Ethan Mallove <mallove@fas.harvard.edu>
2018-09-21 15:57:18 -0400

    Try to import angular-miller-columns app.
    Checkpointing at this error:
    
    ```
    angular.js:15018 ReferenceError: MillerColumnsNodeProvider is not defined
        at new <anonymous> (main.js:31)
        at Object.instantiate (angular.js:5123)
        at $controller (angular.js:11175)
        at Object.link (angular-route.js:1245)
        at angular.js:1364
        at invokeLinkFn (angular.js:10722)
        at nodeLinkFn (angular.js:10099)
        at compositeLinkFn (angular.js:9370)
        at publicLinkFn (angular.js:9235)
        at lazyCompilation (angular.js:9626) "<div ng-view="" class="ng-scope" data-ng-animate="1">"
    ```

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,8 +7,50 @@
  * # MainCtrl
  * Controller of the angular1xMillerColumnsExampleApp
  */
+
 angular.module('angular1xMillerColumnsExampleApp')
   .controller('MainCtrl', function () {
+
+  var nodes = [
+      {
+          id: 'id1',
+          node: 'node1',
+          type: 'leaf',
+          info: {
+              whatever: ['you', 'need']
+          }
+      },
+      {
+          id: 'id2',
+          node: 'node2',
+          type: 'branch'
+      }
+  ];
+   
+  // The node provider must be an instance of MillerColumnsNodeProvider factory
+  this.nodeProvider = new MillerColumnsNodeProvider();
+   
+  // Its getChildrenNodes must return "node" children.
+  // If node === null, you should return root level nodes. The way you retrieve/store your nodes is up to you
+  // You may directly return data or return a promise.
+  this.nodeProvider.getChildrenNodes = function(node) {
+      return $q(function(resolve) {
+          resolve(children);
+      });
+      // OR 
+      // return data;
+  };
+   
+  this.onSelectCallback = function(node) {
+      // Whatever you need...
+  };
+   
+  // You may also inject the MillerColumnsConfiguration value provider in your controller and set some options
+  // Activate column filter (activated by default)
+  MillerColumnsConfiguration.filter = true;
+  // Set filter field placeholder
+  MillerColumnsConfiguration.filterPlaceholder = 'Filter!';
+
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
